9b4e852b2e1e6810214aca0d3f0d3802ed4e7fa4
s-satsangi <s.satsangi@gmail.com>
2020-11-29 16:38:02 -0500

    wow!  this is getting interesting!

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,18 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: birds.map do |bird| 
+    #   bird.slice(:name, :species)
+    # end
+    render json: birds.to_json( except: [:created_at, :updated_at, :id, :species])
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird.slice(:id, :name, :species)
+    else
+      render json: "Hey ya dumbus, there ain't no bird with id: #{params[:id]}"
+    end
   end
 end
